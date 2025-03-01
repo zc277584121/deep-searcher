97df32e74dba06363be226709a8fcb18e061130a
Jonathan Aghachi <aghachi3@gmail.com>
2025-02-28 16:35:51 -0800

    Master (#98)
    * fix test_milvus.py
    
    * removed chunk import

--- tests/test_milvus.py
+++ tests/test_milvus.py
@@ -4,39 +4,42 @@ import numpy as np
 from deepsearcher.vector_db import Milvus, RetrievalResult
 from deepsearcher.tools import log
 
-
 class TestMilvus(unittest.TestCase):
     def test_milvus(self):
         d = 8
-        collection = "hellp_deepsearcher"
+        collection = "hello_deepsearcher"
         milvus = Milvus()
-        milvus.init_db(
+        milvus.init_collection(
             dim=d,
             collection=collection,
         )
         rng = np.random.default_rng(seed=19530)
         milvus.insert_data(
             collection=collection,
-            rows=[
+            chunks=[
                 RetrievalResult(
                     embedding=rng.random((1, d))[0],
                     text="hello world",
                     reference="local file: hi.txt",
+                    metadata={"a": 1},
                 ),
                 RetrievalResult(
                     embedding=rng.random((1, d))[0],
                     text="hello milvus",
                     reference="local file: hi.txt",
+                    metadata={"a": 1},
                 ),
                 RetrievalResult(
                     embedding=rng.random((1, d))[0],
                     text="hello deep learning",
                     reference="local file: hi.txt",
+                    metadata={"a": 1},
                 ),
                 RetrievalResult(
                     embedding=rng.random((1, d))[0],
                     text="hello llm",
                     reference="local file: hi.txt",
+                    metadata={"a": 1},
                 ),
             ],
         )
@@ -47,9 +50,9 @@ class TestMilvus(unittest.TestCase):
 
     def test_clear_collection(self):
         d = 8
-        collection = "deepsearcher"
+        collection = "hello_deepsearcher"
         milvus = Milvus()
-        milvus.init_db(
+        milvus.init_collection(
             dim=d,
             only_init_client=True,
             collection=collection,
